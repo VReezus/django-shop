3ec690c863f63f3f84aa8bb77656dde5465f012f
ПОЛМАТЕРИ <mrsizer22613@gmail.com>
2022-12-08 12:31:53 +0600

    filter + search + page

---
+++ etstes.py
@@ -0,0 +1,11 @@
+import math
+
+t=255
+e=math.e
+g=1e+6
+kg=1000
+c=10
+G=(t*g)
+KG=(t*kg)
+C=(t*c)
+print(f'{G}{KG}{C}')
--- main/views.py
+++ main/views.py
@@ -1,12 +1,42 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from django.db.models import Q
+from rest_framework.response import Response
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
-
+from .filters import ProductFilter 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class=ProductFilter
+    # filterset_fields = ['category', 'status']
+    # permission_classes = [AllowAny, ]
+
+    def get_permissions(self):
+        if self.action in ['retrieve', 'list', 'search']:
+            return []#если это запрос на листинг или детализацию
+        return [IsAdminUser()] #разрешаем всем
+        
+    @action(['GET'], detail=False)
+    def search(self, request):
+        q = request.query_params.get('q')
+        if q:
+            queryset=queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        # queryset = Product.objects.filter(title__icontains=q)
+        pagination = self.paginate_queryset(queryset)
+        
+        if pagination:
+            serializer=self.get_serializer(pagination, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer=self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+
+
+

643cd5e76e3b8b0cebbde69a3f221704ee4b3966
ПОЛМАТЕРИ <mrsizer22613@gmail.com>
2022-12-12 13:00:47 +0600

    average rating + ordering

--- review/permissions.py
+++ review/permissions.py
@@ -3,7 +3,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class isAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             # если просто чтение
